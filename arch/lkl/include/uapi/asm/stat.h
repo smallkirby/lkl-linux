da4435fc4185293db75bebb7959ceb6cffc20649
Hajime Tazaki <thehajime@gmail.com>
2020-03-18 23:40:52 +0900

    lkl: fix struct stat for aarch64
    This commits adds a workaround for struct stat in aarch64 arch.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- arch/lkl/include/uapi/asm/stat.h
+++ arch/lkl/include/uapi/asm/stat.h
@@ -5,7 +5,7 @@
 
 /* copied from arch/x86/include/uapi/asm/stat.h  */
 /* XXX: how to handle other case ? */
-#if __BITS_PER_LONG == 64
+#ifdef __x86_64
 struct stat {
 	unsigned long	st_dev;
 	unsigned long	st_ino;
@@ -34,7 +34,36 @@ struct stat {
 #endif
 	long		__unused[3];
 };
+#else /* !__x86_64 */
+struct stat {
+	unsigned long long st_dev;	/* Device.  */
+	unsigned long long st_ino;	/* File serial number.  */
+	unsigned int	st_mode;	/* File mode.  */
+	unsigned int	st_nlink;	/* Link count.  */
+	unsigned int	st_uid;		/* User ID of the file's owner.  */
+	unsigned int	st_gid;		/* Group ID of the file's group. */
+	unsigned long long st_rdev;	/* Device number, if device.  */
+	unsigned long long __pad1;
+	long long	st_size;	/* Size of file, in bytes.  */
+	int		st_blksize;	/* Optimal block size for I/O.  */
+	int		__pad2;
+	long long	st_blocks;	/* Number 512-byte blocks allocated. */
+#ifdef _KERNEL
+	int		st_atime;	/* Time of last access.  */
+	unsigned int	st_atime_nsec;
+	int		st_mtime;	/* Time of last modification.  */
+	unsigned int	st_mtime_nsec;
+	int		st_ctime;	/* Time of last status change.  */
+	unsigned int	st_ctime_nsec;
+#else
+	struct timespec st_atim;
+	struct timespec st_mtim;
+	struct timespec st_ctim;
 #endif
+	unsigned int	__unused4;
+	unsigned int	__unused5;
+};
+#endif /* !__x86_64 */
 
 /* copied from uapi/asm-generic/stat.h */
 /* This matches struct stat64 in glibc2.1. Only used for 32 bit. */
@@ -67,38 +96,6 @@ struct stat64 {
 	unsigned int	__unused4;
 	unsigned int	__unused5;
 };
-
-/* XXX: need to have same layout for struct stat too.. */
-#if __BITS_PER_LONG == 32
-struct stat {
-	unsigned long long st_dev;	/* Device.  */
-	unsigned long long st_ino;	/* File serial number.  */
-	unsigned int	st_mode;	/* File mode.  */
-	unsigned int	st_nlink;	/* Link count.  */
-	unsigned int	st_uid;		/* User ID of the file's owner.  */
-	unsigned int	st_gid;		/* Group ID of the file's group. */
-	unsigned long long st_rdev;	/* Device number, if device.  */
-	unsigned long long __pad1;
-	long long	st_size;	/* Size of file, in bytes.  */
-	int		st_blksize;	/* Optimal block size for I/O.  */
-	int		__pad2;
-	long long	st_blocks;	/* Number 512-byte blocks allocated. */
-#ifdef _KERNEL
-	int		st_atime;	/* Time of last access.  */
-	unsigned int	st_atime_nsec;
-	int		st_mtime;	/* Time of last modification.  */
-	unsigned int	st_mtime_nsec;
-	int		st_ctime;	/* Time of last status change.  */
-	unsigned int	st_ctime_nsec;
-#else
-	struct timespec st_atim;
-	struct timespec st_mtim;
-	struct timespec st_ctim;
-#endif
-	unsigned int	__unused4;
-	unsigned int	__unused5;
-};
-#endif	/* __BITS_PER_LONG == 32 */
 #endif	/* __BITS_PER_LONG != 64 || defined(__ARCH_WANT_STAT64) */
 
 #endif /* _ASM_UAPI_LKL_STAT_H */
